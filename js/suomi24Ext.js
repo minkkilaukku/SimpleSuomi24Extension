c68b2d736e604669e7c5e8fbf3e230181c2d4c5e
minkkilaukku <39800860+minkkilaukku@users.noreply.github.com>
2019-09-24 13:10:20 +0300

    The way to get the elements has changed
    Also, the original post isn't included in the list of messages the info box cycles through, only the comments.

--- js/suomi24Ext.js
+++ js/suomi24Ext.js
@@ -1,76 +1,39 @@
-var cachedUserEls = [];
-var cachedUserName = null;
-var cachedSortBy = null;
 
-
-/** Get all p.user-info-name elements with given userName (empty means get all)
-*  @param userName, if non-empty, get only messages with this name
-* @param sortBy, if "time" sort by posting time, otherwise in the order they appear on the page
-*/
-var getUserPostElems = function(userName, sortBy) {
-    if (userName === cachedUserName && sortBy === cachedSortBy) {
-        return cachedUserEls;
+/** Get a date from a comment user info container */
+let timeMaker = container => {
+    let tEl = container.querySelector(".ThreadUser__Timestamp-sugcdk-1");
+    if (!tEl) return new Date(0);
+    let tStr = tEl.innerText.trim().toLocaleLowerCase();
+    let year, month, day, hour, minute;
+    let currDate = new Date();
+    if (tStr.substring(0, 6) === "tänään") {
+        year = currDate.getFullYear();
+        month = currDate.getMonth();
+        day = currDate.getDate();
+    } else if (tStr.substring(0, 5) === "eilen") {
+        let yesterDate = new Date(+currDate-86400000) //1000*60*60*24 = one full day
+        year = yesterDate.getFullYear();
+        month = yesterDate.getMonth();
+        day = yesterDate.getDate();
     } else {
-        var userEls = Array.from(document.querySelectorAll("p.user-info-name"));
-        var userNames = userEls.map(x=>x.textContent.trim());
-
-        if (userName.length>0) { //empty userName means get all
-            for (let i=0; i<userNames.length; i++) {
-                if (userNames[i]!==userName) {
-                    delete userEls[i];
-                }
-            }
-            userEls = userEls.filter(x=>x);
-        }
-
-        if (sortBy==="time") {
-            let timeStampGetter = el=>{
-                var tSEl = el.parentElement.getElementsByClassName("user-info-timestamp")[0];
-                if (tSEl) return tSEl.textContent.trim();
-                return "1.1.1900 00:00";
-            };
-            let dateMaker = ts => {
-                var [da, ti] = ts.split(" ");
-                var [day, month, year] = da.split(".");
-                var [hour, minute] = ti.split(":");
-                //month is zero-based (doesn't affect ordering but make it still correct)
-                return new Date(year, month-1, day, hour, minute, 0); //-1 converts to number
-            };
-            //put a date object to each element as property, so it's easy to sort them
-            userEls.forEach( el=>el.datePosted = dateMaker(timeStampGetter(el)) );
-            userEls.sort((a,b)=>a.datePosted-b.datePosted);
-        }
-        
-        cachedUserEls = userEls;
-        cachedUserName = userName;
-        cachedSortBy = sortBy;
-        
-        return userEls;
+        let dateStr = tStr.split(" ")[0];
+        [day, month, year] = dateStr.split(".").map(x=>parseInt(x));
+        month -= 1; //moth is zero-based!
     }
+    let timeStr = tStr.split(" ")[1];
+    [hour, minute] = timeStr.split(":").map(x=>parseInt(x));
+    return new Date(year, month, day, hour, minute);
 };
 
 
-
-/** Get the container parent of a user-name element
-* (this way also the removed messages (where user-info is hidden) will be scrolled to)
-*/
-var getContainerOfUserEl = function(userEl) {
-    //to avoid infinite loop
-    //if for some reason the userEl isn't inside an answer or comment container
-    //it should always be 2 levels up, but find like this to be sure
-    var maxLevelsUp = 4;
-    var levelUpCounter = 0;
-    var res = userEl;
-    while (res && res.classList
-           && !res.classList.contains("answer-container")
-           && !res.classList.contains("comment-container")
-           && levelUpCounter<maxLevelsUp) {
-        res = res.parentElement;
-        levelUpCounter++;
-    }
-    return res;
+/** Array of all user info containers sorted by timestamp */
+let getAllUserInfos = () => {
+    let ret = Array.from(document.querySelectorAll(".CommentListItem__UserInfo-sc-515cm8-1"));
+    ret.forEach(c=>{c.dateForSorting = timeMaker(c);});
+    ret.sort((a,b)=>+a.dateForSorting-b.dateForSorting);
+    //console.log("Got user infos:", ret.map(c=>c.innerText));
+    return ret;
 };
-
 
 
 var showPostIndexInfo = function(postNumber, totalPosts, postContainer) {
@@ -82,46 +45,23 @@ var showPostIndexInfo = function(postNumber, totalPosts, postContainer) {
         "<p class='textPercentInside'>"+Math.round(percent)+"%"+"</p></span>"
         +" Aktiivi viesti "+postNumber+"/"+totalPosts;
     
-    var bdd = postContainer.getBoundingClientRect();
-    var infoBdd = postIndexInfo.getBoundingClientRect();
     postIndexInfo.style.display = "block";
-    postContainer.insertAdjacentElement("afterbegin", postIndexInfo);
-    /*
-    var infoW = Math.max(75, infoBdd.width);
-    postIndexInfo.style.left = (bdd.left-infoW-10)+"px";
-    postIndexInfo.style.top = (bdd.top + window.scrollY)+"px";
-    */
+    postContainer.insertAdjacentElement("afterend", postIndexInfo);
 };
 
 
 /**
-* scroll the page to the @postIndex'th (in the ordering given by @sortBy)
-* post of user @userName (the container of the post is used to give the scroll position).
-* (empty userName means consider all posts)
+* scroll the page to the @postIndex'th user info container, ordering by timestamp
 */
-var scrollToUserPost = function(userName, postIndex, sortBy) {
-    var userEls = getUserPostElems(userName, sortBy);
+var moveInfoToUserPost = function(postIndex) {
+    var userEls = getAllUserInfos();
     var elsN = userEls.length;
     if (elsN>0) {
         var elInd = (postIndex%elsN+elsN)%elsN;
         var el = userEls[elInd];
-        //the first post doesn't have a similar container as answers and comments
-        //so it must be handled separately
-        if (el === document.querySelector("p.user-info-name")) { //the starting post
-            var threadHeader = document.getElementsByClassName("thread-header")[0];
-            if (threadHeader) {
-                var topPos = threadHeader.getBoundingClientRect().top + window.scrollY;
-                window.scrollTo(0, topPos);
-                showPostIndexInfo(elInd+1, elsN, el.parentElement); //use the user-info-big as container
-            }
-        } else {//answers and comments
-            var container = getContainerOfUserEl(el);
-            if (container) {
-                showPostIndexInfo(elInd+1, elsN, container); //this changes the size, so put it in first
-                var topPos = container.getBoundingClientRect().top + window.scrollY;
-                window.scrollTo(0, topPos);
-            }
-        }
+        showPostIndexInfo(elInd+1, elsN, el);
+        var topPos = el.offsetTop; //el.getBoundingClientRect().top + window.scrollY;
+        window.scrollTo(window.scrollX, topPos);
     }
     
 };
@@ -151,6 +91,7 @@ var setKeyboardFindMsgListener = function() {
         
     };
     
+    
     if (ob && ob.useKeyboard) {
         var postInd = 0; //assume want to go to the most recent (with prev button) first
         
@@ -164,8 +105,10 @@ var setKeyboardFindMsgListener = function() {
         
         var onKeyDownFunc = function(postIndIncr, event) {
             postInd += postIndIncr;
-            scrollToUserPost("", postInd, "time");
+            moveInfoToUserPost(postInd);
             event.preventDefault();
+            //console.log("All jutut: ", getAllUserInfos().map(c=>c.dateForSorting));
+    
         };
         var keyListener = function(event) {
             //console.log("key down", event.keyCode);
@@ -199,7 +142,7 @@ postIndexInfo.addEventListener("keydown", function(evt) {
         if (postIndexInfo.getElementsByTagName("input").length) {
             //why won't postIndexInfo.value work here?? Why have to get the input like this:
             postIndex = parseInt(postIndexInfo.getElementsByTagName("input")[0].value)-1;
-            scrollToUserPost(cachedUserName||"", postIndex, cachedSortBy||"time");
+            if (Number.isInteger(postIndex)) moveInfoToUserPost(postIndex);
         }
     }
 });
@@ -219,3 +162,13 @@ postIndexInfo.addEventListener("click", postIndexClickHandler);
 // set keyboard control of finding most recent posts
 setKeyboardFindMsgListener();
 
+
+/* Another method, for putting the comment-body style that should overwrite existing one
+var addStyles = () => {
+    let s = document.createElement("style");
+    s.innerHTML = '.comment-body{resize:vertical;}';
+    document.head.appendChild(s);
+};
+
+addStyles();
+*/
